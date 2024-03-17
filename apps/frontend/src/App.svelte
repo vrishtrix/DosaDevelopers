1ac41cad4e99782837e1df90e1f4f72db38b14d5
Vrishin Patel <vrishin_patel@outlook.com>
2024-03-18 01:16:03 +0530

    add(frontend): View transitions [EXPERIMENTAL]

--- apps/frontend/src/App.svelte
+++ apps/frontend/src/App.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
 	import { Router, Route } from 'svelte-routing';
-	// import { fade } from 'svelte/transition'
+	import { fade } from 'svelte/transition';
 
 	import HomePage from './routes/home.svelte';
 	import LoginPage from './routes/login.svelte';
 	import RegisterPage from './routes/register.svelte';
 
 	const basepath = '/';
+
+	const viewtransition = () => ({
+		fn: fade,
+		duration: 500,
+	});
 </script>
 
-<Router {basepath}>
+<Router {basepath} {viewtransition}>
 	<Route path="/" component="{HomePage}" />
 	<Route path="/login" component="{LoginPage}" />
 	<Route Path="/register" component="{RegisterPage}" />
